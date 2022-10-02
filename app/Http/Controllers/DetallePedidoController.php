6ad3b687148d5fc221454d4e56f098841ba849b6
diegoAguilera11 <diego.aguilera@alumnos.ucn.cl>
2022-10-02 00:22:52 -0300

    first commit Api

---
+++ app/Http/Controllers/DetallePedidoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DetallePedido;
+use App\Http\Requests\StoreDetallePedidoRequest;
+use App\Http\Requests\UpdateDetallePedidoRequest;
+
+class DetallePedidoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreDetallePedidoRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreDetallePedidoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DetallePedido  $detallePedido
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DetallePedido $detallePedido)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\DetallePedido  $detallePedido
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DetallePedido $detallePedido)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateDetallePedidoRequest  $request
+     * @param  \App\Models\DetallePedido  $detallePedido
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateDetallePedidoRequest $request, DetallePedido $detallePedido)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DetallePedido  $detallePedido
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DetallePedido $detallePedido)
+    {
+        //
+    }
+}
