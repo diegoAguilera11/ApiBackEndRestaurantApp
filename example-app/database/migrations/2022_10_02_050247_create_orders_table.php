e8b510630a49a97100e4821f5a038a2e677e320e
Maxur1 <marcelo.soto01@alumnos.ucn.cl>
2022-10-25 19:25:46 -0400

    BaseDeDatos

---
+++ example-app/database/migrations/2022_10_02_050247_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('date', 255);
+            $table->integer('total');
+            $table->integer('status');
+            //$table->enum('statustext', ['CALCULANDO', 'ENPROCESO', 'ENTREGADO']);
+            $table->foreignid('tables_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ example-app/database/migrations/2022_10_02_050521_create_detail_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_orders', function (Blueprint $table) {
+            $table->id('id');
+            $table->integer('Unit_Price');
+            $table->integer('quantity');
+            $table->string('Productcode', 15);
+            $table->foreign('Productcode')->references('code')->on('products');
+            $table->foreignid('orders_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_orders');
+    }
+};
